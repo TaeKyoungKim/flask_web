81d9d7c2509f05bd234762b369c33fbc87d923a9
Gary Kim <moduedu@gmail.com>
2020-07-21 17:39:34 +0900

    mydb.py

--- mydb.py
+++ mydb.py
@@ -13,16 +13,59 @@ cursor = db.cursor()
 #             ENGINE=InnoDB DEFAULT CHARSET=utf8;
 #     '''
 
-sql=''' 
-    CREATE TABLE `topic` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`title` varchar(100) NOT NULL,
-	`body` text NOT NULL,
-	`author` varchar(30) NOT NULL,
-    `create_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (id)
-	) ENGINE=innoDB DEFAULT CHARSET=utf8;
-'''
-cursor.execute(sql)
+# sql=''' 
+#     CREATE TABLE `topic` (
+# 	`id` int(11) NOT NULL AUTO_INCREMENT,
+# 	`title` varchar(100) NOT NULL,
+# 	`body` text NOT NULL,
+# 	`author` varchar(30) NOT NULL,
+#     `create_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+# 	PRIMARY KEY (id)
+# 	) ENGINE=innoDB DEFAULT CHARSET=utf8;
+# '''
+# cursor.execute(sql)
+# db.commit()
+# db.close()
+
+sql_1 = 'SELECT * FROM users;'
+sql_2=  '''
+        INSERT INTO users(name, email , username, password) 
+        VALUES ('PARK' ,'4@naver.com', 'PARK', '1234');
+            '''
+
+# cursor.execute(sql_2)
+# db.commit()
+# db.close()
+# print(result)
+# users = cursor.fetchall()
+# print(users[0][1])
+# cursor.execute(sql_1)
+# users = cursor.fetchall()
+# print(users)
+
+name = 'GANGNAM' 
+email = '6@naver.com'
+username = 'GANGNAM'
+password = '1234'
+sql_3=  '''
+        INSERT INTO users(name, email , username, password) 
+        VALUES (%s ,%s, %s,%s);
+            '''
+# cursor.execute(sql_3, (name, email , username, password))
+# db.commit()
+# db.close()
+
+sql_4='DELETE FROM `users` WHERE  `id`=5;'
+# cursor.execute(sql_4)
+# db.commit()
+# db.close()
+
+sql_5='DELETE FROM users WHERE name="SONG";'
+# cursor.execute(sql_5)
+# db.commit()
+# db.close()
+
+sql_6='UPDATE `users` SET `name`="PARK" WHERE  `id`=6;'
+cursor.execute(sql_6)
 db.commit()
 db.close()
