7ae6e9c04a3f55f22f303dff57c292b2e996b099
Gary Kim <moduedu@gmail.com>
2020-07-22 16:48:35 +0900

    register step 2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask ,render_template , flash , redirect , url_for, session, request, logging
 
 import pymysql
+from passlib.hash import pbkdf2_sha256
 from data import Articles
 
 app = Flask(__name__)
@@ -42,12 +43,12 @@ def register():
         # data = request.body.get('author')
         name = request.form.get('name')
         email = request.form.get('email')
-        password = request.form.get('password')
+        password = pbkdf2_sha256.hash(request.form.get('password'))
         re_password = request.form.get('re_password')
         username = request.form.get('username')
         # name = form.name.data
-        if(password == re_password):
-            print([name, email , password , re_password , username])
+        if(pbkdf2_sha256.verify(re_password,password )):
+            print(pbkdf2_sha256.verify(re_password,password ))
             cursor = db.cursor()
             sql = '''
                 INSERT INTO users (name , email , username , password) 
@@ -71,6 +72,13 @@ def register():
         return render_template('register.html')
 
 
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return "LOGED PAGE"
+    
+    else:
+        return "LOGIN PAGE"
 
 @app.route('/articles', methods=['GET','POST'])
 def articles():
